3a6c86ae7223fbde3cd6d9baa872a478c2f3ae8f
saheeb_tareque <saheebtareque97@gmail.com>
2021-11-05 22:11:13 +0600

    create and read operation done for the meal items

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zuy06.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -11,17 +16,30 @@ async function run() {
       await client.connect();
       console.log('connectet to database')
 
-    //   const meals = client.db("meals");
-    //   const mealsInfo = database.collection("mealsInfo");
-      // create a document to insert
-    //   const doc = {
-    //     title: "Record of a Shriveled Datum",
-    //     content: "No bytes, no problem. Just insert a document, in MongoDB",
-    //   }
-    //   const result = await haiku.insertOne(doc);
-    //   console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      const meals = client.db("meals");
+      const mealsInfo = meals.collection("mealsInfo");
+
+      //Get api
+      app.get('/meals',async(req,res)=>{
+          console.log('hitting it hard');
+          const cursor = mealsInfo.find({});
+          const meals = await cursor.toArray();
+          res.send(meals);
+
+      })
+     
+      //post api
+      app.post('/meals',async(req,res)=>
+      {
+          const mealInfo = req.body;
+          console.log('hitting the post',mealInfo);
+          const result = await mealsInfo.insertOne(mealInfo);
+          console.log(result);
+          res.json(result);
+      });
+
     } finally {
-      await client.close();
+    //   await client.close();
     }
   }
   run().catch(console.dir);
